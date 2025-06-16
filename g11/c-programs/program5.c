16b1f6711506ef6578a1f258194f9114e5d772c1
HKK901 <024neb901@sxc.edu.np>
2025-06-16 21:01:46 +0545

    Add files via upload

--- g11/c-programs/program5.c
+++ g11/c-programs/program5.c
@@ -4,19 +4,19 @@ int main()
     int a, i, t=0;
     printf("Enter a number: ");
     scanf("%d", &a);
-    for ( i = 2; i < a; i++)
+    for ( i = 1; i < a; i++)
     {
         if (a%i==0)
         {
-            t=1;
+            t+=i;
         }
     }
-    if (t=0)
+    if (t==a)
     {
-        printf("\n It is a prime number");
+        printf("\n It is a perfect number");
     }
     else{
-        printf("\n It is a composite number");
+        printf("\n It is not a perfect number");
     }
     printf("\n Program Executed by Harshit");
     return 0;
